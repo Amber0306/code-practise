a661736004b695dd91f9d431768d962302e7a5d7
amber0306 <862268273@qq.com>
2022-04-13 14:48:55 +0800

    553

---
+++ leetcode/python/string/number_change/553.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def optimalDivision(self, nums: List[int]) -> str:
+        # 这题不会做，看题解
+        pass
+
+
+solu = Solution()
+print(solu.optimalDivision([1000,100,10,2]))
